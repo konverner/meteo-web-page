0fb6fd694222778091feac0475f4da63cb2a1543
konverner <konst.verner@gmail.com>
2024-07-14 16:23:03 +0200

    wip

--- src/meteo_web_page/api/data_update.py
+++ src/meteo_web_page/api/data_update.py
@@ -2,6 +2,10 @@ import os
 import requests
 import time
 import logging
+from datetime import datetime
+from typing import List
+
+import pandas as pd
 from dotenv import load_dotenv
 
 from db.database import init_db, save_to_db
@@ -36,6 +40,60 @@ def delete_doc(api_key, doc_id):
     response.raise_for_status()
     return response.json()
 
+
+def prepare_batch(doc_ids: List[str], api_key: str) -> pd.DataFrame:
+    """Run over docs with different measures and prepare batch group by time up to minutes
+    Args:
+        doc_ids: list of document ids
+        api_key: API key for document retrieval
+    Returns:
+        pd.DataFrame: prepared data
+    """
+    data = []
+    for doc_id in doc_ids:
+        doc_content = get_doc_content(api_key, doc_id)
+        if not doc_content:
+            continue
+
+        delete_doc(api_key, doc_id)
+        logger.info(f"Document with id {doc_id} processed and deleted.")
+        time.sleep(0.2)  # To avoid hitting the API rate limit
+
+        head = doc_content.get("head", {})
+        content = doc_content.get("content", {})
+
+        time_str = content.get("time", "")  # 2024-07-14 11:00:00+00
+        if time_str:
+            time_value = datetime.strptime(time_str.replace("+00", ""), "%Y-%m-%d %H:%M:%S")
+            time_value = time_value.replace(second=0, microsecond=0)  # Group by time up to minutes
+        else:
+            continue
+
+        level = content.get("level")
+        wind_speed = content.get("wind_speed")
+        wind_direction = content.get("wind_direction")
+
+        data.append({
+            "datetime": time_value,
+            "level": level,
+            "wind_speed": wind_speed,
+            "wind_direction": wind_direction
+        })
+    if data:
+        # Create a DataFrame
+        df = pd.DataFrame(data)
+    else:
+        df = pd.DataFrame(columns=['datetime', 'level', 'wind_speed', 'wind_direction'])
+
+    # Group by time up to minutes and aggregate the data
+    df_grouped = df.groupby('datetime').agg({
+        'level': 'first',
+        'wind_speed': 'first',
+        'wind_direction': 'first'
+    }).reset_index()
+
+    return df_grouped
+
 def update_database():
     api_key = API_KEY
     conn = init_db()
@@ -45,13 +103,9 @@ def update_database():
         return
     try:
         doc_ids = get_doc_ids(api_key)
-        for doc_id in doc_ids:
-            doc_content = get_doc_content(api_key, doc_id)
-            if doc_content["head"]["title"] == 'р. Большая Нева – Горный институт':
-                save_to_db(conn, doc_id, doc_content)
-            delete_doc(api_key, doc_id)
-            logger.info(f"Document with id {doc_id} processed and deleted.")
-            time.sleep(0.2)  # To avoid hitting the API rate limit
+        batch = prepare_batch(doc_ids, api_key)
+        save_to_db(conn, batch)
+
     finally:
         if conn and conn.is_connected():
             conn.close()
--- src/meteo_web_page/main.py
+++ src/meteo_web_page/main.py
@@ -1,16 +1,23 @@
 # meteo-web-page/src/meteo_web_page/main.py
+import logging
+
 from flask import Flask, jsonify, render_template
 import threading
 import time
 
-from db.database import fetch_data
+from db.database import init_db, fetch_data, drop_table
 from api.data_update import update_database
 
 app = Flask(__name__)
 
+# define logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 @app.route('/data')
 def data():
     data = fetch_data()
+    logger.info(f"Data fetched from the database: {data}")
     return jsonify(data)
 
 @app.route('/')
@@ -20,9 +27,11 @@ def index():
 def update_database_periodically():
     while True:
         update_database()
-        time.sleep(30)  # Sleep for 30 seconds
+        time.sleep(30)
 
 if __name__ == "__main__":
+    drop_table("meteo_observations")
+    init_db()
     update_thread = threading.Thread(target=update_database_periodically, daemon=True)
     update_thread.start()
     app.run(host='0.0.0.0', debug=True)
